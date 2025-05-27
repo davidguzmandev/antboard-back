c925c90cd8b451318f613ece6e01089fc78b2e68
davidruncode <daguz.91@gmail.com>
2025-05-26 20:17:43 -0400

    Auth

---
+++ auth/authController.ts
@@ -0,0 +1,64 @@
+import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import { z } from "zod";
+import User, { Role } from "../users/userModel";
+
+//Esquema ZOD
+const signUpSchema = z.object({
+    role: z.nativeEnum(Role),
+    name: z.string().min(1, "Name is required"),
+    subname: z.string().min(1, "Subname is required"),
+    email: z.string().email("Invalid email format"),
+    password: z.string().min(6, "Password must be at least 6 characters"),
+    companyName: z.string().optional(),
+    phone:z.string().optional(),
+});
+
+//Tipar el body valido
+type SignUpInput = z.infer<typeof signUpSchema>;
+
+export const signUpUser = async (req: Request, res: Response) => {
+    try {
+        //Validar con zod
+        const parsed = signUpSchema.safeParse(req.body)
+        if (!parsed.success) {
+            const errors = parsed.error.flatten().fieldErrors;
+            return res.status(400).json({ error: "Validation failed", details: errors });
+        }
+        const { role, name, subname, email, password, companyName, phone }: SignUpInput = parsed.data;
+
+        // Verificar si el usuario ya existe
+        const existingUser = await User.findOne({ email });
+        if ( existingUser ) {
+            return res.status(409).json({ error: "User already exists" });
+        }
+
+        // Encriptar la contraseña
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        //Crear el usuario
+        const newUser = new User({
+            role,
+            name,
+            subname,
+            email,
+            password: hashedPassword,
+            companyName,
+            phone,
+            isActive: false, // Por defecto, el usuario está inactivo, activar con NODEMAILER
+        })
+
+        await newUser.save();
+        return res.status(201).json({
+            message: "User created successfully",
+            user: {
+                name: newUser.name,
+                subname: newUser.subname,
+                email: newUser.email,
+            },
+        });
+    } catch (error) {
+        console.log("Error in Sign Up: ", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
