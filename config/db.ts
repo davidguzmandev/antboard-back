e83dd1f210a74a1a2c0ce12d66e197a12e6ee8fc
davidruncode <daguz.91@gmail.com>
2025-05-30 20:45:13 -0400

    Prisma y Supabase

--- config/db.ts
+++ config/db.ts
@@ -1,3 +1,37 @@
+import { PrismaClient } from '@prisma/client';
+import dotenv from 'dotenv';
+dotenv.config();
+let prisma: PrismaClient;
+
+declare global {
+    var __prisma: PrismaClient | undefined;
+}
+
+if (process.env.NODE_ENV === 'production') {
+    prisma = new PrismaClient();
+} else {
+    if (!global.__prisma) {
+        global.__prisma = new PrismaClient();
+    }
+    prisma = global.__prisma;
+}
+
+// Funcion de conexion con la DB postgresql con Prisma
+const connectDB = async (): Promise<void> => {
+    try {
+        await prisma.$connect();
+        console.log(`✅ Database connected successfully`);
+    } catch (error) {
+        console.error('❌ Database connection failed:', error);
+        process.exit(1);
+    }
+};
+
+export default prisma;
+export { connectDB };
+
+
+/* CONFIGURACION CON MONGODB
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
@@ -19,4 +53,4 @@ const connectDB = async (): Promise<void> => {
     }
 }
 
-export default connectDB;
+export default connectDB; */
--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import app from '../app';
-import connectDB from './db';
+import {connectDB} from './db';
 
 
 const PORT = process.env.PORT || 3000;
